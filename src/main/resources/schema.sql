c492ce3374e689dbef5780754059226b028fd266
Kirill495 <k495@mail.com>
2023-10-28 17:20:49 +0300

    feat:     добавить условие на существование таблиц.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,15 @@
-CREATE TABLE mpa_rating (
+CREATE TABLE IF NOT EXISTS mpa_rating (
     rating_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title VARCHAR NOT NULL,
     description VARCHAR DEFAULT ''
 );
 
-CREATE TABLE genres (
+CREATE TABLE IF NOT EXISTS genres (
     genre_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title VARCHAR NOT NULL
 );
 
-CREATE TABLE movies (
+CREATE TABLE IF NOT EXISTS movies (
     movie_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title VARCHAR NOT NULL,
     description VARCHAR DEFAULT '',
@@ -18,13 +18,13 @@ CREATE TABLE movies (
     rating INT references mpa_rating(rating_id)
 );
 
-CREATE TABLE movies_genres (
+CREATE TABLE IF NOT EXISTS movies_genres (
     movie_id INT REFERENCES movies(movie_id),
     genre_id INT REFERENCES genres(genre_id),
     PRIMARY KEY (movie_id, genre_id)
 );
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
   user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR NOT NULL UNIQUE,
   login VARCHAR NOT NULL UNIQUE,
@@ -32,14 +32,14 @@ CREATE TABLE users (
   birthday DATE
 );
 
-CREATE TABLE user_relations (
+CREATE TABLE IF NOT EXISTS user_relations (
     first_friend_id INT REFERENCES users(user_id),
     second_friend_id INT REFERENCES users(user_id),
     accepted boolean NOT NULL DEFAULT FALSE,
     PRIMARY KEY (first_friend_id, second_friend_id)
 );
 
-CREATE TABLE movies_likes (
+CREATE TABLE IF NOT EXISTS movies_likes (
     movie_id INT REFERENCES movies(movie_id),
     user_id INT REFERENCES users(user_id),
     PRIMARY KEY (movie_id, user_id)
